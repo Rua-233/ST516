e185b6fbef1d87ec89618a229302c70bab6f54fe
Rua-233 <60053376+Rua-233@users.noreply.github.com>
2021-09-02 23:31:50 -0400

    HW1 DONE!!!

--- HW1.R
+++ HW1.R
@@ -14,7 +14,6 @@ muYx = predict(slm, new = grid, interval = "confidence")
 muYx 
 AirvPred = predict(slm, new = grid, interval = "prediction")
 AirvPred
-dev.new(width=10, height=8, unit="cm")
 layout(matrix(c(1,2,3,4),1,1)) 
 plot(grid$dielectric_constant, muYx[,1], type = "l", lwd = 2, 
      xlab = "dielectric constant", ylab = "air void (%)", ylim = c(0,10))
@@ -43,8 +42,10 @@ plot(airplane$horsepower, airplane$max_speed_mph, pch = 19)
 test_dt = sample(1:79,20,replace=F, set.seed(10))
 train = airplane[-test_dt,]
 test = airplane[test_dt,]
-
+plot(test$horsepower, test$max_speed_mph)
+## build and fit models
 fit1 <- lm(max_speed_mph ~ horsepower, data = train)
+summary(fit1)
 
 fit2 <- lm(max_speed_mph ~ poly(horsepower, degree = 2, raw = T), data = train)
 summary(fit2)
@@ -83,23 +84,39 @@ layout(matrix(c(1,2,3,4),2,2))
 plot(fit7)
 print(anova(fit1, fit2, fit3, fit4, fit5, fit6))
 
-
-fit3.1 <- lm(max_speed_mph ~ horsepower + I(horsepower^2) + I(horsepower^3), data = train)
-summary(fit3.1)
-coef(summary(fit3.1))
+## choose the model with degree = 2 to improve
+fit2.1 <- lm(max_speed_mph ~ horsepower + I(horsepower^2), data = train)
+summary(fit2.1)
+coef(summary(fit2.1))
 layout(matrix(c(1,2,3,4),2,2)) 
 plot(fit2.1)
-prediction <- predict(fit3.1, test)
-layout(matrix(c(1,2,3,4),1,1)) 
-plot(test$horsepower, test$max_speed_mph)
-lines(smooth.spline(test$horsepower,prediction), col = "blue", lwd = 3)
+prediction <- predict(fit2.1, test)
+muYx_ap <- predict(fit2.1, test, interval = "confidence")
+muYx_ap
+pred_ap <- predict(fit2.1, test, interval = "prediction")
+pred_ap
+layout(matrix(c(1,2,3,4),1,1)) # smooth.spline(test$max_speed_mph, muYx_ap[,1]
+## plot with 95% CI and 95% PI
+plot(test$horsepower, test$max_speed_mph, xlim = c(100, 500), ylim = c(100, 300), xlab = "Horsepower", ylab = "Max Speed (mph)")
+lines(smooth.spline(test$horsepower, muYx_ap[,1]), lty = 1, lwd = 2)
+lines(smooth.spline(test$horsepower, pred_ap[,2]), lty = 2, lwd = 2, col = "red")
+lines(smooth.spline(test$horsepower, pred_ap[,3]), lty = 2, lwd = 2, col = "red")
+lines(smooth.spline(test$horsepower, muYx_ap[,2]), lty = 3)
+lines(smooth.spline(test$horsepower, muYx_ap[,3]), lty = 3)
+points(test$horsepower, test$max_speed_mph, pch = 19, col = "blue")
+legend("topleft", legend = c("fit", "95% CI", "95% PI", "actual"), 
+       lwd = c(2,1,1,NA), pch = c(NA,NA,NA,19), lty = c(1,2,3,NA), col = c("black", "black", "red", "blue"), 
+       bty = "n")
 summary(prediction)
+## calculate the mse
 data <- data.frame(pred = prediction, actual = test$max_speed_mph)
 mse <- mean((data$actual - data$pred)^2)
 mse
 #coconut
 
 ## Q.a
+install.packages("faraway")
+library(faraway)
 fibers <- read.csv("fiber.csv", header = T)
 pairs(fibers)
 plot(fibers)
@@ -109,12 +126,12 @@ cor(fibers) #strong positive correlation found between "grad" and "vel", positiv
 mr_fit1 <- lm(vel ~ cont + lngth + grad, data = fibers)
 plot(mr_fit1)
 summary(mr_fit1)
-
+vif(mr_fit1)
 ## model 2
 mr_fit2 <- lm(vel ~ grad + lngth, data = fibers)
 plot(mr_fit2)
 summary(mr_fit2)
-
+vif(mr_fit2)
 ## model 3
 mr_fit3 <- lm( vel ~ grad + lngth + cont 
                + I(cont^2) + I(lngth^2) + I(grad^2) 
@@ -122,7 +139,7 @@ mr_fit3 <- lm( vel ~ grad + lngth + cont
                data = fibers)
 plot(mr_fit3)
 summary(mr_fit3)
-
+vif(mr_fit3)
 ## model 4
 fibers$cont_c <- fibers$cont - mean(fibers$cont)
 fibers$lngth_c <- fibers$lngth - mean(fibers$lngth)
@@ -132,10 +149,11 @@ mr_fit4 <- lm( vel ~ grad + lngth + cont + I(cont_c^2) + I(lngth_c^2) + I(grad_c
                data = fibers)
 plot(mr_fit4)
 summary(mr_fit4)
-
+vif(mr_fit4)
 ## model 5
 mr_fit5 <- lm( vel ~ grad + lngth + I(lngth_c^2) + I(grad_c^2) 
                + cont_c*lngth_c + lngth_c*grad_c, 
                data = fibers)
 plot(mr_fit5)
 summary(mr_fit5)
+vif(mr_fit5)
